6b1dc6539c0466bb363659e7399a0892bc4bbdbb
minadss <813352937@qq.com>
2020-02-07 12:26:06 +0800

    项目路由调整

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
-    
+    <router-link to='info'>测试一下</router-link>
   </div>
 </template>
 
 <script>
 export default {
-
+data() {
+  return {
+    
+  }
+},
 }
 </script>
 
---
+++ src/router/index.js
@@ -0,0 +1,11 @@
+//导入
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+//注册
+Vue.use(VueRouter)
+//实例化
+const router = new VueRouter({
+    routes: []
+})
+//暴露出去
+export default router;
