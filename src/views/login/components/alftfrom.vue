0f62c34340aa94a636c5f409b533a4b95cc44335
minadss <813352937@qq.com>
2020-02-09 08:57:22 +0800

    登录页面布局

---
+++ src/views/login/components/alftfrom.vue
@@ -0,0 +1,85 @@
+<template>
+  <el-dialog class="alftfrom-dialog" center width="603px" title="用户注册" :visible.sync="dialogFormVisible">
+
+    <el-form :model="form">
+      <el-form-item label="昵称" :label-width="formLabelWidth">
+        <el-input v-model="form.name" autocomplete="off"></el-input>
+      </el-form-item>
+    
+      <el-form-item label="邮箱" :label-width="formLabelWidth">
+        <el-input v-model="form.name" autocomplete="off"></el-input>
+      </el-form-item>
+   
+      <el-form-item label="手机" :label-width="formLabelWidth">
+        <el-input v-model="form.name" autocomplete="off"></el-input>
+      </el-form-item>
+ 
+      <el-form-item label="密码" :label-width="formLabelWidth">
+        <el-input v-model="form.name" autocomplete="off"></el-input>
+      </el-form-item>
+      
+  <!-- 图形码 -->
+      <el-form-item label="图形码" :label-width="formLabelWidth">
+          <el-row>
+              <el-col :span="17">
+                  <el-input v-model="form.name" autocomplete="off"></el-input>
+              </el-col>
+              <el-col :span="7" class="register-box">
+                  <img class="register-code" src="../../../assets/login-code.png" alt="">
+              </el-col>
+          </el-row>
+      </el-form-item>
+      <!-- 验证码 -->
+      <el-form-item label="验证码" :label-width="formLabelWidth">
+          <el-row>
+              <el-col :span="17">
+                  <el-input v-model="form.name" autocomplete="off"></el-input>
+              </el-col>
+              <el-col :span="7">
+                 <el-button>点击获取验证码</el-button>
+              </el-col>
+          </el-row>
+      </el-form-item>
+    </el-form>
+    <!-- 点击框 -->
+    <div slot="footer" class="dialog-footer">
+      <el-button @click="dialogFormVisible = false">取 消</el-button>
+      <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+    </div>
+  </el-dialog>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    //是否显示对话框
+      dialogFormVisible: false,
+      //表单属性
+      form: {
+        name: ""
+      },
+      //文本宽度
+      formLabelWidth: "62px"
+    };
+  }
+};
+</script>
+
+<style lang='less'>
+.alftfrom-dialog {
+  .el-dialog__header {
+    background: linear-gradient(to right, #0dbfef, #1792f8);
+  }
+  .el-dialog__title {
+    color: white;
+  }
+  .register-box{
+    height: 40.8px;
+  }
+  .register-code{
+    height: 40.8px;
+    width: 100%;
+  }
+}
+</style>
--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -7,24 +7,32 @@
         <span class="left-shu"></span>
         <span class="left-name">用户登录</span>
       </div>
-      <el-form ref="form" :model="loginform" label-width="43px">
+      <el-form ref="loginform" :model="loginform" :rules="rules" label-width="43px">
+          <!-- 手机号 -->
         <el-form-item>
           <el-input placeholder="请输入手机号" v-model="loginform.phone" prefix-icon="el-icon-user"></el-input>
         </el-form-item>
-        <el-form-item>
-          <el-input placeholder="请输入密码" v-model="loginform.passwold" prefix-icon="el-icon-lock"></el-input>
+        <!-- 密码 -->
+        <el-form-item prop="password">
+          <el-input placeholder="请输入密码" v-model="loginform.password" prefix-icon="el-icon-lock"></el-input>
         </el-form-item>
+        <!-- 验证码 -->
+        <el-form-item prop="logincode">
         <el-row>
-          <el-col :span="17">
-            <el-input placeholder="请输入验证码" v-model="loginform.logincode" prefix-icon="el-icon-key" class="logincode"></el-input>
-             
+          <el-col :span="17" >
+            <el-input
+              placeholder="请输入验证码"
+              v-model="loginform.logincode"
+              prefix-icon="el-icon-key"
+              class="logincode"
+            ></el-input>
           </el-col>
           <el-col :span="7">
-           <img class="imglogin" src="../../assets/login-code.png" alt="">
+            <img class="imglogin" src="../../assets/login-code.png" alt />
           </el-col>
-        
         </el-row>
-        
+        </el-form-item>
+        <!-- 多选框 -->
         <el-form-item>
           <el-checkbox v-model="loginform.ischecked">
             我已阅读并同意
@@ -33,28 +41,66 @@
           </el-checkbox>
         </el-form-item>
         <el-form-item>
-          <el-button class="my-btn" type="primary">登录</el-button>
-          <el-button class="my-btn" type="primary">注册</el-button>
-         
+            <!-- 按钮 -->
+          <el-button class="my-btn" type="primary" @click="submitForm('loginform')">登录</el-button>
+          <el-button class="my-btn" type="primary" @click="showAlftfrom">注册</el-button>
         </el-form-item>
       </el-form>
     </div>
     <img src="../../assets/login_banner_ele.png" alt />
+    <!-- 子组件标签 -->
+     <alftfrom ref="alftfrom"></alftfrom>
   </div>
 </template>
 
 <script>
+//导入子组件地址
+import alftfrom from './components/alftfrom'
+
 export default {
   name: "login",
+  //注册
+  components:{
+      alftfrom
+  },
   data() {
     return {
       loginform: {
         phone: "",
-        passwold: "",
+        password: "",
         ischecked: false,
-        logincode: "",
+        logincode: ""
+      },
+      //非空判断
+      rules: {
+        password: [
+          { required: true, message: "请输入密码", trigger: "blur" },
+          { min: 6, max: 12, message: "密码的长度6-12位", trigger: "blur" }
+        ],
+        logincode: [
+          { required: true, message: "请输入验证码", trigger: "blur" },
+          { min: 4, max: 4, message: "验证码的长度4位", trigger: "blur" }
+        ]
       }
     };
+  },
+  methods: {
+      //表单验证
+    submitForm(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          this.$message.success("验证成功");
+        } else {
+          
+          this.$message.error("验证失败");
+          return false;
+        }
+      });
+    },
+    showAlftfrom(){
+        //调用子组件的方式显示出表单
+        this.$refs.alftfrom.dialogFormVisible = true
+    }
   }
 };
 </script>
@@ -70,29 +116,28 @@ export default {
   display: flex;
   align-items: center;
   justify-content: space-around;
- 
+
   .left-box {
     width: 478px;
     height: 550px;
     background-color: #f5f5f5;
-      .el-checkbox{
+    .el-checkbox {
       display: flex;
       align-items: center;
-      .el-checkbox__label{
-          display: flex;
-          
+      .el-checkbox__label {
+        display: flex;
       }
-      .el-checkbox__inner{
-          display: flex;
+      .el-checkbox__inner {
+        display: flex;
       }
-  }
-     .el-form{
-         height: 100%;
-          box-sizing:border-box;
-     }
-    .el-form-item{
+    }
+    .el-form {
+      height: 100%;
+      box-sizing: border-box;
+    }
+    .el-form-item {
       margin-bottom: 0;
-  }
+    }
     .left-top {
       display: flex;
       align-items: center;
@@ -119,35 +164,30 @@ export default {
         height: 24px;
         background-color: #c9c9c9;
       }
-     
     }
-     .my-btn{
-          display: block;
-          width: 394px;
-          height: 40px;
-          margin-left: 0;
-          margin-bottom: 28px;
-      }
-  
-    .imglogin{
-    // width: 100%;
+    .my-btn {
+      display: block;
+      width: 394px;
+      height: 40px;
+      margin-left: 0;
+      margin-bottom: 28px;
+    }
+
+    .imglogin {
+      width: 100%;
       height: 40.8px;
     }
   }
- 
-  .logincode{
-      margin-left: 43px;
-  }
-  .el-row{
-      margin-bottom: 32px;
-      width: 394px;
-  }
-  .el-form-item__content{
-     
-      width: 394px;
-      height: 43px;
-      margin-bottom: 26px;
-  }
 
+  
+  .el-row {
+    margin-bottom: 32px;
+    width: 394px;
+  }
+  .el-form-item__content {
+    width: 394px;
+    height: 43px;
+    margin-bottom: 26px;
+  }
 }
 </style>
