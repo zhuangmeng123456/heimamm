559875368a6b4dc68afa213da6bb2c70e9290cd8
minadss <813352937@qq.com>
2020-02-12 15:31:11 +0800

    主页面的所有分页完成

--- src/router/index.js
+++ src/router/index.js
@@ -3,8 +3,21 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import login from '../views/login/login.vue'
 import index from '../views/index/index.vue'
+import chart from '../views/index/chart/chart.vue'
+import user from '../views/index/user/user.vue'
+import question from '../views/index/question/question.vue'
+import enterprise from '../views/index/enterprise/enterprise.vue'
+import subject from '../views/index/subject/subject.vue'
+
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 //注册
 Vue.use(VueRouter)
+
+
+
+
+
 //实例化
 const router = new VueRouter({
     //路由配置
@@ -15,9 +28,41 @@ const router = new VueRouter({
         },
         {
             path:'/index',
-            component:index
-        }
+            component:index,
+            children:[
+                {
+                path:'chart',
+                component:chart
+                },
+                {
+                path:'user',
+                component:user
+                },
+                {
+                path:'question',
+                component:question
+                },
+                {
+                path:'enterprise',
+                component:enterprise
+                },
+                {
+                path:'subject',
+                component:subject
+                },
+            ]
+        },
     ]
+})
+
+
+router.beforeEach((to,from,next)=>{
+    NProgress.start()
+    next()
+})
+
+router.afterEach(()=>{
+    NProgress.done()
 })
 //暴露出去
 export default router;
--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -2,19 +2,49 @@
   <el-container class="my-container">
     <el-header class="my-header">
       <div class="left">
-        <i class="el-icon-s-fold"></i>
+        <i @click="isCollapse=!isCollapse" class="el-icon-s-fold"></i>
         <img src="..//../assets/login_logos.png" alt="" />
         <span>黑马面面</span>
       </div>
       <div class="right">
         <img :src="userIcon" alt="" />
         <span class="name">{{username}},您好</span>
-        <el-button type="primary">退出</el-button>
+        <el-button type="primary" @click="loginout">退出</el-button>
       </div>
     </el-header>
     <el-container>
-      <el-aside width="200px" class="my-aside">Aside</el-aside>
-      <el-main class="my-main">Main</el-main>
+      <el-aside width="auto" class="my-aside">
+       <el-col :span="14" >
+    <el-menu router
+     :collapse="isCollapse"
+      :default-active="$route.path"
+      class="el-menu-vertical-demo">
+      <el-menu-item index="/index/chart">
+        <i class="el-icon-pie-chart"></i>
+        <span slot="title">数据概览</span>
+      </el-menu-item>
+      <el-menu-item index="/index/user">
+        <i class="el-icon-user"></i>
+        <span slot="title">用户列表</span>
+      </el-menu-item>
+      <el-menu-item index="/index/question">
+        <i class="el-icon-edit-outline"></i>
+        <span slot="title">题库列表</span>
+      </el-menu-item>
+      <el-menu-item index="/index/enterprise">
+        <i class="el-icon-office-building"></i>
+        <span slot="title">企业列表</span>
+      </el-menu-item>
+      <el-menu-item index="/index/subject">
+        <i class="el-icon-notebook-2"></i>
+        <span slot="title">学科列表</span>
+      </el-menu-item>
+    </el-menu>
+  </el-col>
+      </el-aside>
+      <el-main class="my-main">
+        <router-view></router-view>
+      </el-main>
     </el-container>
   </el-container>
 </template>
@@ -22,7 +52,9 @@
 <script>
 // 导入接口
 // import { info } from '../../api/index.js';
-import { info } from '../../api/index'
+import { info,logout } from '../../api/index'
+import { romveToken } from '@/utils/token.js'
+
 export default {
   name: 'index',
   data(){
@@ -30,18 +62,37 @@ export default {
       // 用户名
       username:"",
       // 用户头像
-      userIcon:""
+      userIcon:"",
+      isCollapse:false
     }
   },
   created() {
     info().then(res=>{
-        window.console.log(res)
+      
       this.username = res.data.data.username;
       // 服务器返回的头像地址不完整，需要进行拼接
       this.userIcon =  process.env.VUE_APP_URL+"/"+res.data.data.avatar
     })
-  }
+  },
+  methods:{
+    loginout(){
+       this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+          confirmButtonText: '狠心离开',
+          cancelButtonText: '继续留下',
+          type: 'warning'
+        }).then(() => {
+          logout().then(res=>{
+            if(res.data.code===200){
+              romveToken();
+              this.$router.push('/login')
+            }
+          })
+        })
+          .catch(() => { })
+    }
+}
 };
+
 </script>
 
 <style lang="less">
@@ -79,8 +130,12 @@ export default {
       }
     }
   }
+ .el-menu-vertical-demo:not(.el-menu--collapse) {
+    width: 200px;
+    min-height: 400px;
+  }
   .my-aside {
-    background: yellowgreen;
+   
   }
   .my-main {
     background: #0094ff;
